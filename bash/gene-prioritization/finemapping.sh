829d082ab2920a91e61265c13d56a6c166f21f51
ampregnall <ampregnall@gmail.com>
2026-08-28 15:44:05 -0400

    feat: ABF finemapping with tiled loci

---
+++ bash/gene-prioritization/finemapping.sh
@@ -0,0 +1,22 @@
+#BSUB -J "finemapping[1-5]"
+#BSUB -n 1
+#BSUB -R "rusage[mem=24GB]"
+#BSUB cwd /project/damrauer_copd/copd-asthma-gwas-nf/
+#BSUB -W 1:00
+#BSUB -N
+#BSUB -q voltron_normal
+
+set -euo pipefail
+
+POPS=(
+  "all"
+  "afr"
+  "eur"
+  "eas"
+  "amr"
+)
+POP="${POPS[$LSB_JOBINDEX - 1]}"
+
+pixi run Rscript src/finemapping/finemapping.R \
+    --sumstats data/meta-analysis/copd/copd-"${POP}".txt.gz \
+    --loci data/tiled-loci/copd-"${POP}"-tiled-loci.txt 
--- src/finemapping/finemapping.R
+++ src/finemapping/finemapping.R
@@ -1,66 +1,67 @@
 source("src/finemapping/tiled_locus_extract.R")
 source("src/finemapping/finemapping-utils.R")
 library(data.table)
+library(argparse)
+library(glue)
 
-populations <- c("eur", "afr", "amr", "eas", "all")
+# Add command line arguments
+parser <- ArgumentParser()
+parser$add_argument(
+    "--sumstats",
+    help = "Path to summary statistic file"
+)
+parser$add_argument(
+    "--loci",
+    help = "Path to locus definition file"
+)
 
-purrr::map(populations, function(x) {
-    df_sumstats <- fread(paste0("data/meta-analysis/copd/copd-", x, ".txt.gz"))
+args <- parser$parse_args()
 
-    # Extract lead variants and loci using tiling method
-    df_loci <- extract_loci_tiled(
-        df = df_sumstats,
-        chr_col = CHR,
-        pos_col = POS,
-        p_col = P,
-        snp_col = SNPID,
-        beta_col = BETA,
-        se_col = SE,
-        z_col = Z
-    )
+df_sumstats <- fread(args$sumstats)
+df_loci <- fread(args$loci)
 
-    # Subset summary statistics to only include variants within the extracted loci
-    df_sumstats <- locus_extract_dtable(
-        sumstats_df = df_sumstats,
-        locus_df = df_loci,
-        sumstats_chr_col = CHR,
-        sumstats_pos_col = POS,
-        locus_chr_col = chr,
-        locus_start_col = start,
-        locus_end_col = end
-    )
+# Subset summary statistics to only include variants within the extracted loci
+df_sumstats <- locus_extract_dtable(
+    sumstats_df = df_sumstats,
+    locus_df = df_loci,
+    sumstats_chr_col = CHR,
+    sumstats_pos_col = POS,
+    locus_chr_col = CHR,
+    locus_start_col = BP_MIN,
+    locus_end_col = BP_MAX
+)
 
-    # Calculate credible sets for each locus using approximate Bayes factor method
-    df_credset <- calc_credset(
-        df = df_sumstats,
-        locus_marker_col = locus_marker,
-        effect_col = BETA,
-        se_col = SE,
-        samplesize_col = N,
-    )
+# Calculate credible sets for each locus using approximate Bayes factor method
+df_credset <- calc_credset(
+    df = df_sumstats,
+    locus_marker_col = locus_marker,
+    effect_col = BETA,
+    se_col = SE,
+    samplesize_col = N,
+)
 
-    df_credset <- df_credset |>
-        dplyr::rename(
-            LOCUS = locus_marker,
-            CHR = chromosome,
-            START = start,
-            END = end,
-            BF = bf,
-            PIP = posterior_prob
-        ) |>
-        dplyr::mutate(LEAD = SNPID %in% df_loci$lead_snp) |>
-        dplyr::arrange(CHR, START, -PIP)
+df_credset <- df_credset |>
+    dplyr::rename(
+        LOCUS = locus_marker,
+        CHR = chromosome,
+        START = start,
+        END = end,
+        BF = bf,
+        PIP = posterior_prob
+    ) |>
+    dplyr::mutate(LEAD = rsID %in% df_loci$LEAD_SNP) |>
+    dplyr::arrange(CHR, START, -PIP)
 
-    df_lead <- df_credset |> dplyr::filter(LEAD)
+out_dir <- "data/credible-sets"
+out_prefix <- gsub(
+    pattern = ".txt.gz",
+    replacement = "",
+    x = basename(args$sumstats)
+)
+dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
 
-    fwrite(
-        df_credset,
-        glue::glue("data/credible-sets/copd-{x}-credible-sets.txt"),
-        sep = "\t"
-    )
-    fwrite(
-        df_lead,
-        glue::glue("data/lead-variants/copd-{x}-lead-variants.txt"),
-        sep = "\t"
-    )
-})
+fwrite(
+    df_credset,
+    glue("{out_dir}/{out_prefix}-credible-sets.txt"),
+    sep = "\t"
+)
